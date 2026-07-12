2ade9d3275ad4a6b11dd968a805e8d11c33f475a
DaniLlP <84320501+DaniLlP@users.noreply.github.com>
2026-07-12 12:13:44 +0300

    Initialize Supabase client in supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+const supabaseUrl = "https://twelmnurbonnttwtsxzk.supabase.co";
+
+const supabaseKey = "sb_publishable_uihaTd8UB0mnddI4YC8gMw_qQJmMDwI";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
